0e13e40e40214a72e278acb1e05168bcaa3c0a82
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-08-22 12:56:43 +0200

    Enhance code readability

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,14 +10,18 @@ import menuItems from "../../constants/menuItems";
 import "./navbar.css";
 
 export default function Navbar() {
-    console.log(menuItems)
     return (
         <nav className="navbar">
             <ul>
                 {menuItems.map((item, index) => (
-                    <li key={index} className="nav-item">
-                        <Link to={`/${item.path}`} className="nav-link link-1">
-                            {/* {item.path.charAt(0).toUpperCase() + item.path.slice(1)} */}
+                    <li 
+                        key={index} 
+                        className="nav-item"
+                    >
+                        <Link 
+                            to={`/${item.path}`} 
+                            className="nav-link link-1"
+                        >
                             {firstLetterToUpperCase(item.path)}
                         </Link>
                     </li>
