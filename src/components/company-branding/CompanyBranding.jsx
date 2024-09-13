6767c7178ef975fa4f7ae984a9b954cb1a0886e1
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-09-13 13:19:14 +0200

    Enhance code readability

--- src/components/company-branding/CompanyBranding.jsx
+++ src/components/company-branding/CompanyBranding.jsx
@@ -7,16 +7,17 @@ import "./company-branding.css";
 export default function CompanyBranding() {
     return (
         <div className="company-branding">
-            <h1
-                className="heading-1"
-            >
+            {/* Company name */}
+            <h1 className="heading-1">
                 Trebla Services
             </h1>
+            {/* Company description */}
             <div className="company-description">
                 <p className="paragraph-2">
                     Thermografie & Luchtdichtheid<br />
                     by Vanderbilt
                 </p>
+                {/* Company logo */}
                 <div className="logo-wrapper">
                     <img
                         src={companyLogo}
