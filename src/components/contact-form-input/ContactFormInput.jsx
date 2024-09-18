317b7e4cd759b2e38400154d5f32001baa62fb81
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-09-18 09:08:58 +0200

    Cleanup code

--- src/components/contact-form-input/ContactFormInput.jsx
+++ src/components/contact-form-input/ContactFormInput.jsx
@@ -8,35 +8,30 @@ export default function ContactFormInput({ inputField, formState, setFormState, 
     const type = (inputField === "from_name" && "text") || (inputField === "reply_to" && "email") || (inputField === "message" && null);
     const placeholder = (inputField === "from_name" && "Naam") || (inputField === "reply_to" && "Email") || (inputField === "message" && "Bericht");
 
-    return (
-        <>
-            {
-                inputField === "message" ?
-                    <textarea
-                        name={inputField}
-                        id={inputField}
-                        placeholder={placeholder}
-                        required
-                        onChange={(e) => handleInputChange(e, formState, setFormState, setSanitizationError)}
-                    >
-                    </textarea>
-                    :
-                    <input
-                        type={type}
-                        name={inputField}
-                        id={inputField}
-                        placeholder={placeholder}
-                        required
-                        onChange={(e) => handleInputChange(e, formState, setFormState, setSanitizationError)}
-                    />
-            }
-            {
-  (sanitizationError[inputField] || validationError[inputField]) && (
-    <p className="paragraph-1 error">
-      {sanitizationError[inputField] || validationError[inputField]}
-    </p>
-  )
-}
-        </>
-    )
+    const inputFieldProps = {
+        type: type,
+        name: inputField,
+        id: inputField,
+        placeholder: placeholder,
+        required: true,
+        onChange: (e) => handleInputChange(e, formState, setFormState, setSanitizationError)
+    }
+
+return (
+    <>
+        {
+            inputField === "message" ?
+                <textarea {...inputFieldProps}></textarea>
+                :
+                <input {...inputFieldProps} />
+        }
+        {
+            (sanitizationError[inputField] || validationError[inputField]) && (
+                <p className="paragraph-1 error">
+                    {sanitizationError[inputField] || validationError[inputField]}
+                </p>
+            )
+        }
+    </>
+)
 }
