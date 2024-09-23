8c31e2e4f52fee58efd9975acb19752bf3f2004f
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-09-23 09:33:02 +0200

    Add footer

--- src/components/page-footer/PageFooter.jsx
+++ src/components/page-footer/PageFooter.jsx
@@ -1,10 +1,36 @@
+import { Link } from "react-router-dom";
+
+// Constants
+import legalPageData from "../../constants/legalPageData";
+
+// Helpers
+import firstLetterToUpperCase from "../../helpers/firstLetterToUpperCase";
+import getCurrentYear from "../../helpers/getCurrentYear";
+
 // Styles
 import "./page-footer.css";
 
 export default function PageFooter() {
+  const navItems = Object.keys(legalPageData);
+
   return (
     <footer className="page-footer">
-        Page footer
+      <ul>
+        {navItems.map((navItem, index) => (
+          <li
+            key={index}
+            className="nav-item"
+          >
+            <Link
+              to={`/${navItem}`}
+              className="link-2"
+            >
+              {firstLetterToUpperCase(navItem)}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <p className="paragraph-4">Trebla Services {getCurrentYear()}</p>
     </footer>
   )
 }
