c0df47314447415e7e507f6da4976694b21432af
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-09-24 10:27:58 +0200

    Add not found content

---
+++ src/constants/notFoundPageData.jsx
@@ -0,0 +1,15 @@
+const notFoundPageData = 
+    {
+        title: "Oeps! Pagina niet gevonden",
+        textContent: [
+            "Het lijkt erop dat je verdwaald bent. Geen zorgen, we helpen je graag weer op weg.",
+            "De pagina die je zoekt, bestaat niet (meer) of is verplaatst. Probeer een van de volgende opties:",
+            [
+                "Ga terug naar de {homepage} en ontdek onze diensten",
+                "Neem contact met ons op via ons {contactformulier} als je vragen hebt."
+            ],
+            "We hopen je snel weer op de juiste weg te zien!"
+        ]
+    }
+
+export default notFoundPageData;
--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,30 +1,72 @@
-import { useHeader } from "../../context/HeaderContext";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-// Animations
-import slideInOut from "../../animations/slide-in-out";
+// Context
+import { useHeader } from "../../context/HeaderContext";
 
 // Hooks
 import useScrollToTop from "../../hooks/useScrollToTop";
+
+// Components
+import Paragraph from "../../components/paragraph/Paragraph";
+
+// Constants
+import notFoundPageData from "../../constants/notFoundPageData";
+
+// Animations
+import slideInOut from "../../animations/slide-in-out";
 
 // Styles
 import "./not-found.css";
 
 export default function NotFound() {
   useScrollToTop();
-  
+
+  const { title, textContent } = notFoundPageData;
+
   const { headerHeight } = useHeader();
 
   return (
-    <motion.main 
+    <motion.main
       className="not-found"
       style={{ paddingTop: headerHeight }}
       variants={slideInOut}
       initial="hidden"
       animate="visible"
-      exit="exit" 
+      exit="exit"
     >
-        Not Found
+      <h2 className="heading-2">
+        {title}
+      </h2>
+      <div className="content-container">
+        {
+          textContent.map((item) => {
+            return typeof (item) === "object" ?
+              <ul key={item}>
+                {
+                  item.map((subitem) => {
+                    const isHomepage = subitem.includes("{homepage}");
+                    const [before, after] = subitem.split(isHomepage ? "{homepage}" : "{contactformulier}");
+                  
+                    return (
+                      <li key={subitem} className="paragraph-1">
+                        {before}
+                          <Link to={isHomepage ? "/" : "/contact"}>
+                            {isHomepage ? "homepage" : "contactformulier"}
+                          </Link>
+                        {after}
+                      </li>
+                    );
+                  })
+                }
+              </ul>
+              :
+              <Paragraph key={item}>
+                {item}
+              </Paragraph>
+          })
+        }
+      </div>
     </motion.main>
   )
 }
